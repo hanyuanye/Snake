41122a9b4e1e8a9e05b4b4960fe5e37a02a25ab4
Hanyuan Ye <hanyuan.ye@uwaterloo.ca>
2019-07-03 18:42:23 -0400

    first commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Hanyuan Ye on 2019-07-03.
+//  Copyright © 2019 HY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
